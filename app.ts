efde49ef32ab77a82af2610e1e757ecb620eec50
joussy <chootman@hotmail.fr>
2017-04-22 15:58:10 +0200

    - Travis file added - First unit test added - Express library removed

--- app.ts
+++ app.ts
@@ -1,20 +1,26 @@
 ﻿import * as BoxRec from './boxrec';
 import * as BoutsToIcs from './bouts-to-ics';
-var express = require('express');
+const http = require('http');
 var boutsToIcs = new BoutsToIcs.BoxerToIcs();
+var url = require('url');
 
-var app = express();
 
-app.get('/', function (req, res) {
-    res.header("Content-Type", 'text/plain');
-    var ids = req.query.boxerids.split(',');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    console.log(port);
+    res.setHeader('Content-Type', 'text/plain');
+    var params = url.parse(req.url, true).query;
+    if (params.boxerids == null)
+        return res.end('Error');
+    var ids = params.boxerids.split(',');
     boutsToIcs.fromIds(ids).then(data => {
-        res.send(data)
+        res.end(data)
     }).catch(x => {
-        res.send("Error")
+        res.end("Error")
     });
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
