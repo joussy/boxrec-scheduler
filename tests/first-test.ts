49bce159da66537c4fec26f3ba07d2714c4c0228
joussy <chootman@hotmail.fr>
2017-04-23 14:41:49 +0200

    tests added

---
+++ tests/first-test.ts
@@ -0,0 +1,10 @@
+﻿const chaiLib = require('chai');
+////import 'mocha';
+var expect = chaiLib.expect;
+//var CartSummary = require('./../../src/part1/cart-summary');
+describe('CartSummary', function () {
+    it('res.length should be 3', function () {
+        var res = [1, 2, 3];
+        expect(res.length).to.equal(3);
+    });
+});
